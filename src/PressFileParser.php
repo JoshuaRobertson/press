a45a001d807b69ddf58923f8a10dba2e66281515
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-09-25 15:17:47 +0100

    Added test blog and PressFileParser to get the post content

---
+++ src/PressFileParser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace JoshuaRobertson\press;
+
+use File;
+
+class PressFileParser
+{
+  protected $filename;
+  protected $data;
+
+  public function __construct($filename)
+  {
+    $this->filename = $filename;
+
+    $this->splitFile();
+  }
+
+  public function getData()
+  {
+    return $this->data;
+  }
+
+  protected function splitFile()
+  {
+    preg_match('/^\-{3}(.*?)\-{3}(.*)/s',
+      File::get($this->filename),
+      $this->data
+    );
+  }
+}
