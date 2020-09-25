89f327dc38f85ecaf62711169dd62257b19cd8ae
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-09-25 15:38:21 +0100

    Parsed post body content

--- src/PressFileParser.php
+++ src/PressFileParser.php
@@ -14,6 +14,8 @@ class PressFileParser
     $this->filename = $filename;
 
     $this->splitFile();
+
+    $this->explodeData();
   }
 
   public function getData()
@@ -27,5 +29,16 @@ class PressFileParser
       File::get($this->filename),
       $this->data
     );
+  }
+
+  protected function explodeData()
+  {
+    foreach (explode("\n", trim($this->data[1])) as $fieldString) {
+      preg_match('/(.*):\s?(.*)/', $fieldString, $fieldArray);
+
+      $this->data[$fieldArray[1]] = $fieldArray[2];
+    }
+
+    $this->data['body'] = trim($this->data[2]);
   }
 }
