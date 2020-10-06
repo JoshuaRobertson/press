4abaff1ca415524a63563864d1e40c61854f64d4
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-10-06 14:40:26 +0100

    Refactored PostRepository to handle extra fields

--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -4,6 +4,7 @@ namespace JoshuaRobertson\press\Repositories;
 
 use JoshuaRobertson\press\Post;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PostRepository
 {
@@ -15,7 +16,15 @@ class PostRepository
       'slug' => Str::slug($post['title']),
       'title' => $post['title'],
       'body' => $post['body'],
-      'extra' => $post['extra'] ?? json_encode([]),
+      'extra' => $this->extra($post),
     ]);
+  }
+
+  private function extra($post)
+  {
+    $extra = (array)json_decode($post['extra'] ?? '[]');
+    $attributes = Arr::except($post, ['identifier', 'title', 'body', 'extra']);
+
+    return json_encode(array_merge($extra, $attributes));
   }
 }
